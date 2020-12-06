d288a400f4f3a25afd7bab8ce9753d4caa55bb11
NickRylander <nick.rylander@outlook.com>
2020-12-06 11:18:18 -0800

    commit tree 91e23a25736ca462f51e60048fe8ee66472e7948

--- index.js
+++ index.js
@@ -20,3 +20,55 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = function(thisArr) {
+    return {
+        firstName: thisArr[0],
+        familyName: thisArr[1],
+        title: thisArr[2],
+        payPerHour: thisArr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeData) {
+    return employeeData.map(function(row) {
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateStamp) {
+    let inTime = this.timeInEvents.find(function(e) {
+        return e.date === dateStamp
+    })
+    let outTime = this.timeOutEvents.find(function(e) {
+        return e.date === dateStamp
+    })
+    return (outTime.hour - inTime.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateStamp) {
+    let payOwed = hoursWorkedOnDate.call(this, dateStamp) *this.payPerHour
+    return payOwed
+}
